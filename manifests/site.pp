08045f2ebe89565d1ca9dc3e13ba18cfde971a13
David Cunningham <davidcunningham@c02nk20ag3qp.home>
2014-12-06 15:14:18 -0500

    changed site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -53,10 +53,10 @@ Homebrew::Formula <| |> -> Package <| |>
 
 node default {
   # core modules, needed for most things
-  include dnsmasq
-  include git
-  include hub
-  include nginx
+  #include dnsmasq
+  #include git
+  #include hub
+  #include nginx
 
   # fail if FDE is not enabled
   if $::root_encrypted == 'no' {
@@ -64,25 +64,25 @@ node default {
   }
 
   # node versions
-  nodejs::version { 'v0.6': }
-  nodejs::version { 'v0.8': }
-  nodejs::version { 'v0.10': }
+  #nodejs::version { 'v0.6': }
+  #nodejs::version { 'v0.8': }
+  #nodejs::version { 'v0.10': }
 
   # default ruby versions
-  ruby::version { '1.9.3': }
-  ruby::version { '2.0.0': }
-  ruby::version { '2.1.0': }
-  ruby::version { '2.1.1': }
-  ruby::version { '2.1.2': }
+  #ruby::version { '1.9.3': }
+  #ruby::version { '2.0.0': }
+  #ruby::version { '2.1.0': }
+  #ruby::version { '2.1.1': }
+  #ruby::version { '2.1.2': }
 
   # common, useful packages
-  package {
-    [
-      'ack',
-      'findutils',
-      'gnu-tar'
-    ]:
-  }
+  #package {
+  #  [
+  #    'ack',
+  #    'findutils',
+  #    'gnu-tar'
+  #  ]:
+  #}
 
   file { "${boxen::config::srcdir}/our-boxen":
     ensure => link,
